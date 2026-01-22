425d50feb8ffe15fd9c9bfa98b002bd76ceeb893
DEK-DEBIAN <dek.other@pm.me>
2026-01-22 20:17:53 +1100

    Updated run.py to reference ./src

--- run.py
+++ run.py
@@ -2,7 +2,7 @@
 """Run the Equipment Controller application."""
 
 import sys
-from equipment_controller.main import main
+from src.main import main
 
 if __name__ == "__main__":
     sys.exit(main())
